0094873076c4b4c866a2aa14f5c7705920ad86eb
AdminIEDGE <133484025+AdminIEDGE@users.noreply.github.com>
2023-10-25 11:37:24 +0500

    Initial commit

---
+++ assets/data/dummydata.js
@@ -0,0 +1,223 @@
+import { AiOutlineCalendar } from "react-icons/ai"
+import { TfiRulerPencil } from "react-icons/tfi"
+import { VscFileSubmodule } from "react-icons/vsc"
+import { BiUser } from "react-icons/bi"
+
+export const home = [
+  {
+    icon: <AiOutlineCalendar size={25} />,
+    title: "10+ years of market experience",
+  },
+  {
+    icon: <TfiRulerPencil size={25} />,
+    title: "Unique technologies & modern approach ",
+  },
+  {
+    icon: <VscFileSubmodule size={25} />,
+    title: "100+ cuccessful cases in portfolio",
+  },
+  {
+    icon: <BiUser size={25} />,
+    title: "Customer satisfaction is our top priority",
+  },
+]
+export const expertise = [
+  {
+    id: 1,
+    title: "Destinations",
+    cover: "../images/e1.jpg",
+    desc: [{ text: "Planning Destinations" }, { text: "Finding Destinations" }, { text: "Customised Pakage" }, { text: "Hosting" }, { text: "Website audit" }],
+  },
+  {
+    id: 2,
+    title: "Hotels",
+    cover: "../images/e2.jpg",
+    desc: [{ text: "Hotels suggestion" }, { text: "Booking" }, { text: "Finding Hotels" }, { text: "Hotels" }],
+  },
+  {
+    id: 3,
+    title: "Flights",
+    cover: "../images/e3.jpg",
+    desc: [{ text: "Flights Booking" }, { text: "Booking" }, { text: "Flights management" }, { text: "Flights" }],
+  },
+  {
+    id: 4,
+    title: "Building trip from scratch",
+    cover: "../images/e4.jpg",
+    desc: [{ text: "Planing trip" }, { text: "Trip management" }, { text: "Planning Trip" }],
+  },
+]
+export const testimonial = [
+  {
+    id: 1,
+    name: "Harrison Ford",
+    cover: "../images/userPic1.jpg",
+    post: "Seven consulting - CEO",
+    desc: "Nunc fermentum - tempus erat ligula, sit amet lacinia justo cursus ac. Suspendisse quis nulla tincidunt! Lorem ipsum dolor amet at ornare ex, quis fringilla tortor! Nunc consectetur feugiat rutrum. Sed rhoncus sapien!",
+  },
+  {
+    id: 2,
+    name: "Diana Green",
+    cover: "../images/userPic2.jpg",
+    post: "Seven Arts - marketing manager",
+    desc: "Cras at ornare fermentum quam et tortor euismod, vel maximus metus tristique at ornare ex, quis fringilla tortor. Aenean semper neque quis consectetur lobortis. Quisque nec convallis ex. Aenean ut metus et nunc cursus aliquet.",
+  },
+  {
+    id: 3,
+    name: "Alexander Black",
+    cover: "../images/userPic3.jpg",
+    post: "Seven consulting - CEO",
+    desc: "Nunc fermentum - tempus erat ligula, sit amet lacinia justo cursus ac. Suspendisse quis nulla tincidunt! Lorem ipsum dolor amet at ornare ex, quis fringilla tortor! Nunc consectetur feugiat rutrum. Sed rhoncus sapien!",
+  },
+  {
+    id: 4,
+    name: "Diana Green",
+    cover: "../images/userPic4.jpg",
+    post: "Seven Arts - marketing manager",
+    desc: "Cras at ornare fermentum quam et tortor euismod, vel maximus metus tristique at ornare ex, quis fringilla tortor. Aenean semper neque quis consectetur lobortis. Quisque nec convallis ex. Aenean ut metus et nunc cursus aliquet.",
+  },
+]
+export const showcase = [
+  {
+    id: 1,
+    title: "Neon Lights",
+    cover: "../images/s1.jpg",
+    catgeory: "DIGITAL MARKETING",
+  },
+  {
+    id: 2,
+    title: "Futuristic Furniture",
+    catgeory: "WEBSITES",
+    cover: "../images/s2.jpg",
+  },
+  {
+    id: 3,
+    title: "Smart Living",
+    cover: "../images/s3.jpg",
+    catgeory: "WEBSITES",
+  },
+  {
+    id: 4,
+    title: "Light Painting",
+    cover: "../images/s4.jpg",
+    catgeory: "BRANDING	",
+  },
+  {
+    id: 5,
+    title: "Ideabox",
+    cover: "../images/s5.jpg",
+    catgeory: "BRANDING	",
+  },
+  {
+    id: 5,
+    title: "VR Experience",
+    cover: "../images/s6.jpg",
+    catgeory: "WEBSITES	",
+  },
+]
+export const brand = [
+  {
+    id: 1,
+    cover: "../images/l1.svg",
+  },
+  {
+    id: 2,
+    cover: "../images/l2.svg",
+  },
+  {
+    id: 3,
+    cover: "../images/l3.svg",
+  },
+  {
+    id: 4,
+    cover: "../images/l4.svg",
+  },
+  {
+    id: 5,
+    cover: "../images/l5.svg",
+  },
+  {
+    id: 6,
+    cover: "../images/l6.svg",
+  },
+]
+export const blogdata = [
+  {
+    id: 1,
+    title: "The best fall hikes in the Adirondacks",
+    cover: "../images/b1.jpg",
+    catgeory: "INDUSTRY",
+    date: "JANUARY 12, 2023",
+  },
+  {
+    id: 2,
+    title: "Don’t underestimate the lorem ipsum dolor amet",
+    cover: "../images/b2.jpg",
+    catgeory: "TIPS & TRICKS",
+    date: "OCTOBER 20, 2023",
+  },
+  {
+    id: 3,
+    title: "If you’re looking for a short hike…",
+    cover: "../images/b3.jpg",
+    catgeory: "TIPS & TRICKS",
+    date: "OCTOBER 9, 2023",
+  },
+  {
+    id: 4,
+    title: "8 of America’s coolest mountain towns",
+    cover: "../images/b4.jpg",
+    catgeory: "EVENTS",
+    date: "OCTOBER 8, 2023",
+  },
+]
+export const teamdata = [
+  {
+    id: 1,
+    title: "Alexander Black",
+    cover: "../images/t1.jpg",
+    post: "FOUNDER, CEO",
+  },
+  {
+    id: 2,
+    title: "Anna Kovalenko",
+    cover: "../images/t2.jpg",
+    post: "FINANCE DIRECTOR",
+  },
+  {
+    id: 3,
+    title: "Tiffany White",
+    cover: "../images/t3.jpg",
+    post: "CREATIVE DIRECTOR",
+  },
+  {
+    id: 4,
+    title: "Richard Greenwood",
+    cover: "../images/t4.jpg",
+    post: "PROGRAMMER",
+  },
+  {
+    id: 5,
+    title: "Jessica Brown",
+    cover: "../images/t5.jpg",
+    post: "MARKETING DIRECTOR",
+  },
+  {
+    id: 6,
+    title: "Gregory Windstorm",
+    cover: "../images/t6.jpg",
+    post: "ACCOUNTING MANAGER",
+  },
+  {
+    id: 7,
+    title: "Anna Red",
+    cover: "../images/t7.jpg",
+    post: "PROJECT MANAGER",
+  },
+  {
+    id: 8,
+    title: "Join our team!",
+    cover: "../images/t8.jpg",
+    post: "",
+  },
+]
